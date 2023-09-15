dbc3793802ed640f4afd25ae9e331bca4adda82c
bahittinn <bahittin84@gmail.com>
2023-09-15 21:59:04 +0300

    play audio

--- Background-Modes/ViewController.swift
+++ Background-Modes/ViewController.swift
@@ -6,18 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureAudio()
+    }
+    
+    private func configureAudio() {
+        do {
+            let filePath = Bundle.main.path(forResource: "music", ofType: "mp3")
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: filePath!))
+            audioPlayer.prepareToPlay()
+            
+        } catch {
+            print("DEBUG: error is \(error.localizedDescription)")
+        }
     }
 
     @IBAction func startButtonClicked(_ sender: UIButton) {
+        audioPlayer.play()
     }
     
     @IBAction func stopButtonClicked(_ sender: UIButton) {
+        audioPlayer.stop()
     }
 }
 
